e2a106bbbf221d00e0ed01b0935164fb3d3486d5
lya4943 <lyall.rogers@gmail.com>
2019-10-17 13:37:21 -0400

    Partially cleaned up region

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pandas as pd
 loans = pd.read_csv('loan_final313.csv')
 loans = loans.drop(['id', 'issue_d', 'final_d', 'home_ownership', 'application_type', 'income_category', 'purpose',
                     'interest_payments', 'loan_condition', 'grade', 'term'], axis=1)
-print(loans.head())
-print(loans.info())
+loans = loans.replace('munster', 0)
+loans = loans.replace('leinster', 1)
+loans = loans.replace('cannught', 2)
+loans = loans.replace('ulster', 3)
+print(loans['region'])
 # Lyall
