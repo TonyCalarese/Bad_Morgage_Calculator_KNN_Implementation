6c937c1b1c7403b64026145ec83dd7cce7a9d7d5
TonyCalarese <accalarese@gmail.com>
2019-10-22 18:46:49 -0400

    More Comments

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ from sklearn.datasets import load_iris
 #print(loans['region'])
 # Lyall
 
-#Tony Start of Code
+
 class Loans:
     #Initialize with the file name and the column that y will be,
     def __init__(self, file, y_delimiter):
@@ -55,50 +55,45 @@ class Loans:
         for col in NewData.columns: # If the column attributes are objects then append the encoded version of those Otherwise append the data
             self.X[col] = X_encoder.fit_transform(NewData[col]) if NewData[col].dtype == 'O' else NewData[col]
 
-        #Memory Clean up
-        del data_df, X_encoder, y_encoder, y_delimiter, NewData
+        del data_df, X_encoder, y_encoder, y_delimiter, NewData #Memory Clean up Tony
 
     #Function to set k, default is 3
     def setK(self, k):
-        self.k = k
+        self.k = k #Tony
+
     #Function to set TestSize if you would like to, default is 70% 30%
     def setTestSize(self, testSize):
-        self.testSize = testSize
-
-    def showKeys(self):
-        print(self.X.keys())
+        self.testSize = testSize #Tony
 
     #Running the sklearn KNN
     def sklearnKNN(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, stratify=self.y, test_size=self.testSize, random_state=21)
-        knn = KNeighborsClassifier(n_neighbors=self.k)
-        knn.fit(X_train, y_train)
-        y_pred = knn.predict(X_test)
-        print(knn.score(X_test, y_test))
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, stratify=self.y, test_size=self.testSize, random_state=21) #Tony and John
+        knn = KNeighborsClassifier(n_neighbors=self.k) #Tony
+        knn.fit(X_train, y_train) #Tony
+        y_pred = knn.predict(X_test) #Tony
+        print(knn.score(X_test, y_test)) #Tony
 
 
     #This will take a list of variables that will be used to filter out of X
     def filterOut(self, columns):
-        self.X.drop(columns, inplace=True, axis=1)
+        self.X.drop(columns, inplace=True, axis=1) #Lyall
 
     #Function for showing what X and y are
     def showAll(self):
-        print(self.X)
-        print(self.y)
-#Tony End
+        print(self.X) #Tony
+        print(self.y) #Tony
 
-#Tony Begin
 #Main Function
 if __name__== "__main__":
+    #Tony
     loan1 = Loans(file='loan_final313.csv', y_delimiter='loan_condition_cat')
-    #loan1.showKeys()
+
     #Lyall
     loan1.filterOut(['issue_d', 'final_d', 'home_ownership', 'application_type',
                      'income_category', 'purpose', 'interest_payments', 'grade', 'term', 'id',
                      'loan_condition'])
     #Tony
     #loan1.showAll()
-    loan1.setK(3)
-    loan1.sklearnKNN()
+    loan1.setK(3) #Tony
+    loan1.sklearnKNN() #Tony
 
-#Tony End
